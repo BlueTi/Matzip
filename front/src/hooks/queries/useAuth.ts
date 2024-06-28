a24db6deaa2ed2cc6032a4a567f8af97c43eea79
JaeHo <dlwp142@gmail.com>
2024-06-28 22:26:32 +0900

    4-4완료 커스텀 드로어 추가

--- front/src/hooks/queries/useAuth.ts
+++ front/src/hooks/queries/useAuth.ts
@@ -1,6 +1,7 @@
 import {useEffect} from 'react';
 import {useMutation, useQuery} from '@tanstack/react-query';
 import {
+  ResponseProfile,
   getAccessToken,
   getProfile,
   logout,
@@ -73,7 +74,7 @@ function useGetRefreshToken() {
   return {isSuccess, isError};
 }
 
-function useGetProfile(queryOptions?: UseQueryCustomOptions) {
+function useGetProfile(queryOptions?: UseQueryCustomOptions<ResponseProfile>){
   return useQuery({
     queryKey: [queryKeys.AUTH, queryKeys.GET_PROFILE],
     queryFn: getProfile,
--- front/src/navigations/drawer/MainDrawerNavigator.tsx
+++ front/src/navigations/drawer/MainDrawerNavigator.tsx
@@ -1,9 +1,12 @@
-import {mainNavigiations} from '@/constants';
+import {colors, mainNavigiations} from '@/constants';
 import {createDrawerNavigator} from '@react-navigation/drawer';
-import MapStackNavigator, { MapStackParamList } from '../stack/MapStackNavigator';
+import MapStackNavigator, {MapStackParamList} from '../stack/MapStackNavigator';
 import FeedHomeScreen from '@/screens/feed/FeedHomeScreen';
 import CalendarHomeScreen from '@/screens/calendar/CalendarHomeScreen';
-import { NavigatorScreenParams } from '@react-navigation/native';
+import {NavigatorScreenParams, RouteProp} from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {Dimensions} from 'react-native';
+import CustomDrawerContent from './CustomDrawerContent';
 
 export type MainDrawerParamList = {
   [mainNavigiations.HOME]: NavigatorScreenParams<MapStackParamList>;
@@ -13,17 +16,57 @@ export type MainDrawerParamList = {
 
 const Drawer = createDrawerNavigator<MainDrawerParamList>();
 
+function DrawerIcons(route: RouteProp<MainDrawerParamList>, focused: boolean) {
+  let iconName = '';
+
+  switch (route.name) {
+    case mainNavigiations.HOME: {
+      iconName = 'location-on';
+      break;
+    }
+    case mainNavigiations.FEED: {
+      iconName = 'book';
+      break;
+    }
+    case mainNavigiations.CALENDAR: {
+      iconName = 'event-note';
+      break;
+    }
+  }
+
+  return (
+    <MaterialIcons
+      name={iconName}
+      size={18}
+      color={focused ? colors.BLACK : colors.GRAY_500}
+    />
+  );
+}
+
 function MainDrawerNavigator() {
   return (
     <Drawer.Navigator
-      screenOptions={{
+      drawerContent={CustomDrawerContent}
+      screenOptions={({route}) => ({
         headerShown: false,
         drawerType: 'front',
-      }}>
+        drawerStyle: {
+          width: Dimensions.get('screen').width * 0.6,
+          backgroundColor: colors.WHITE,
+        },
+        drawerActiveTintColor: colors.BLACK,
+        drawerInactiveTintColor: colors.GRAY_500,
+        drawerActiveBackgroundColor: colors.PINK_500,
+        drawerInactiveBackgroundColor: colors.GRAY_200,
+        drawerLabelStyle: {
+          fontWeight: '600',
+        },
+        drawerIcon: ({focused}) => DrawerIcons(route, focused),
+      })}>
       <Drawer.Screen
         name={mainNavigiations.HOME}
         component={MapStackNavigator}
-        options={{title: '홈'}}
+        options={{title: '홈', swipeEnabled: false}}
       />
       <Drawer.Screen
         name={mainNavigiations.FEED}
